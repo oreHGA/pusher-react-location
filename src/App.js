50e4eae82f4f8bf5b6594b89b5bee5dfbe2619e5
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2018-12-18 05:52:03 +0000

    loading bing maps with single center specified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactBingmaps } from 'react-bingmaps'
 
 class App extends Component {
+  static defaultProps = {
+    center: [5.6219868, -0.1733074],
+    pushPins: [
+      {
+        "location": [5.6219868, -0.1733074],
+        "option": { color: "red", title: "Accra Mall", subTitle: "Your location" }
+      }
+    ],
+    zoom: 11
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ReactBingmaps
+        bingmapKey="YOUR BING MAPS KEY"
+        center={this.props.center}
+        mapTypeId={"road"}
+        zoom={15}
+        pushPins={this.props.pushPins}
+      >
+      </ReactBingmaps>
     );
   }
 }
